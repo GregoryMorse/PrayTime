4e65be9a3ad46fcf19a149610bfc5baddecbdb9b
GregoryMorse <gregory.morse@live.com>
2014-03-30 00:56:22 +0800

    Initial commit

---
+++ sample.cs
@@ -0,0 +1,31 @@
+using System;
+using PrayTime;
+
+// Pray Times Example
+// By: Jandost Khoso
+
+class Program
+{
+    static void Main ( string [ ] args )
+    {
+        PrayTime.PrayTime p = new PrayTime.PrayTime();
+        double lo = 25;
+        double la = 55;
+        int y = 0 , m = 0 , d = 0 , tz = 0;
+
+        DateTime cc = DateTime.Now;
+        y = cc.Year;
+        m = cc.Month;
+        d = cc.Day;
+        tz = TimeZone.CurrentTimeZone.GetUtcOffset(new DateTime (y,m,d)).Hours;
+        String [] s ;
+
+        p.setCalcMethod ( 2 );
+        p . setAsrMethod ( 0 );
+        s = p . getDatePrayerTimes ( y , m , d , lo , la , tz, 0 );
+        for(int i = 0 ; i < s.Length ; ++i )
+        {
+            Console . WriteLine ( s [ i ] );
+        }
+    }
+}
