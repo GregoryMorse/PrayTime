c283c27eb164f600a7adb97206a4cf5906de6779
Gregory Morse <gregory.morse@live.com>
2016-03-26 18:46:54 +0100

    More general PCL

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,17 +10,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PrayTime")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52f683cc-063c-41f7-8d4d-13db14b1471d")]
+//[assembly: Guid("52f683cc-063c-41f7-8d4d-13db14b1471d")]
 
 // Version information for an assembly consists of the following four values:
 //
